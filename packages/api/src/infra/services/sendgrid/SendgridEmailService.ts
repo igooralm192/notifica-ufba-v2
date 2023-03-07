26b0c7564c675397d1243670d520b351058f9025
Igor de Almeida <igoor1205@gmail.com>
2023-03-06 23:50:19 -0300

    feat: add sendgrid email service

---
+++ packages/api/src/infra/services/sendgrid/SendgridEmailService.ts
@@ -0,0 +1,29 @@
+import { IEmailService } from '@/data/contracts'
+import sendgrid, {ClientResponse} from '@sendgrid/mail'
+
+export class SendgridEmailService implements IEmailService.SendMail {
+  constructor(
+    API_KEY: string
+  ) {
+    sendgrid.setApiKey(API_KEY)
+    console.log({API_KEY})
+  }
+
+  async sendMail({
+    to,
+    subject,
+    body,
+  }: IEmailService.SendMail.Input): Promise<void> {
+    try {
+      await sendgrid.send({
+        from: 'igor.nascimento@ufba.br',
+        to,
+        subject,
+        text: body,
+        html: body,
+      })
+    } catch (error) {
+      console.error(error.response.body)
+    }
+  }
+}
--- packages/api/src/main/factories/services/EmailServiceFactory.ts
+++ packages/api/src/main/factories/services/EmailServiceFactory.ts
@@ -1,10 +1,11 @@
 import { IEmailService } from '@/data/contracts'
-import { NodemailerEmailService } from '@/infra/services/nodemailer/NodemailerEmailService'
+import { SendgridEmailService } from '@/infra/services/sendgrid/SendgridEmailService'
 
 import env from '@/main/config/env'
 
 type EmailService = IEmailService.SendMail
 
 export const makeEmailService = (): EmailService => {
-  return new NodemailerEmailService(env.SMTP_HOST, env.SMTP_PORT, env.SMTP_AUTH_USER, env.SMTP_AUTH_PASSWORD)
+  // return new NodemailerEmailService(env.SMTP_HOST, env.SMTP_PORT, env.SMTP_AUTH_USER, env.SMTP_AUTH_PASSWORD)
+  return new SendgridEmailService(env.SENDGRID_API_KEY)
 }
